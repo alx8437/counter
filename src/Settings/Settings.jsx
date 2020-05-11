a0e8bb465d5f9140914880f89a56012e014d5be4
Alx8437 <alx8437@mail.ru>
2020-05-11 11:02:15 +0300

    set error value

--- src/Settings/Settings.jsx
+++ src/Settings/Settings.jsx
@@ -6,7 +6,8 @@ class Settings extends React.Component {
 
     state = {
         maxValue: 0,
-        startValue: 0
+        startValue: 0,
+        error: false
     }
 
     newMaxValueInput = (e) => {
@@ -20,6 +21,11 @@ class Settings extends React.Component {
     onNewValueConter = () => {
         let min = Number(this.state.startValue);
         let max = Number(this.state.maxValue);
+        if ((min > max) || (min < 0) || (max < 0) || (min === max)) {
+            this.setState({
+                error: true
+            })
+        }
         this.props.onNewValueConter(min, max)
     }
 
@@ -28,18 +34,20 @@ class Settings extends React.Component {
 
     render = () => {
 
+        let classToError = this.state.error === true ? `${styles.inputError}` : `${styles.input}`
+
 
         return (
             <div className={styles.wrapper}>
                 <div className={styles.settings}>
                     <span>start value</span>
-                    <input
+                    <input className={classToError}
                         value={this.state.startValue}
                         type="number"
                         onChange={this.newMinValueInput}
                     />
                     <span>max value</span>
-                    <input
+                    <input className={classToError}
                         value={this.state.maxValue}
                         onChange={this.newMaxValueInput}
                         type="number"
